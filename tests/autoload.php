cd35566355b29c35af7b13cd51550e67258fbbc0
Derek Herman <derek.herman@xwp.co>
2018-10-06 00:39:58 -0700

    Fix typo

--- tests/autoload.php
+++ tests/autoload.php
@@ -17,12 +17,12 @@ if ( file_exists( $config ) ) {
 
 // VVV Paths.
 $autoload_path  = dirname( __FILE__ );
-$wp_test_config = substr( $autoload_path, 0, strpos( $autoload_path, 'public_html' ) + 11 ) . '/wp-tests-config.php';
+$wp_tests_config = substr( $autoload_path, 0, strpos( $autoload_path, 'public_html' ) + 11 ) . '/wp-tests-config.php';
 
 /**
  * Supports loading the wp-tests-config.php from the `public_html` root directory of both the
  * `wordpress-default` and `wordpress-develop` sites, and any other custom site in the www directory.
  */
-if ( file_exists( $wp_test_config ) ) {
-	include_once $wp_test_config;
+if ( file_exists( $wp_tests_config ) ) {
+	include_once $wp_tests_config;
 }
