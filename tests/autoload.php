0dbca1b3826455117c41c1e8a16a9803c56e7cfb
Derek Herman <derek.herman@xwp.co>
2018-10-06 00:36:52 -0700

    Fix phpcs issue

--- tests/autoload.php
+++ tests/autoload.php
@@ -16,13 +16,13 @@ if ( file_exists( $config ) ) {
 }
 
 // VVV Paths.
-$path = dirname( __FILE__ );
-$file = substr( $path, 0, strpos( $path, 'public_html' ) + 11 ) . '/wp-tests-config.php';
+$autoload_path  = dirname( __FILE__ );
+$wp_test_config = substr( $autoload_path, 0, strpos( $autoload_path, 'public_html' ) + 11 ) . '/wp-tests-config.php';
 
 /**
  * Supports loading the wp-tests-config.php from the `public_html` root directory of both the
  * `wordpress-default` and `wordpress-develop` sites, and any other custom site in the www directory.
  */
-if ( file_exists( $file ) ) {
-	include_once $file;
+if ( file_exists( $wp_test_config ) ) {
+	include_once $wp_test_config;
 }
