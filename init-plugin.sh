ff83e29cf51dbaeeda53dca1e0b7a88f411efa7f
Derek Herman <derek.herman@xwp.co>
2020-02-05 15:08:40 -0800

    Make interactive

--- init-plugin.sh
+++ init-plugin.sh
@@ -1,37 +1,78 @@
 #!/bin/bash
-# Usage: ./foo-bar/init-plugin.sh xwp "Hello World"
-# Creates a directory "hello-world" in the current working directory,
-# performing substitutions on the scaffold "foo-bar" plugin at https://github.com/xwp/wp-foo-bar
+# Usage: ./wp-foo-bar/init-plugin.sh
 
-set -e
+# Check for valid plugin name.
+function valid_name () {
+	valid="^[A-Z][A-Za-z0-9]*( [A-Z][A-Za-z0-9]*)*$"
 
-if [ $# != 2 ]; then
-	echo "You must supply two arguments, the github username or organization and plugin name."
-	exit 1
-fi
+	if [[ ! "$1" =~ $valid ]]; then
+		return 1
+	fi
 
-user="$1"
-if [ -z "$user" ]; then
-	echo "Provide github username or organization argument"
-	exit 1
-fi
+	return 0
+}
 
-name="$2"
-if [ -z "$name" ]; then
-	echo "Provide name argument"
-	exit 1
-fi
+echo
+echo "Hello, "$USER"."
+echo
+echo "This script will automatically generate a new plugin based on the scaffolding."
+echo "The way it works is you enter a plugin name like 'Hello World' and the script "
+echo "will create a directory 'hello-world' in the current working directory, while "
+echo "performing substitutions on the 'wp-foo-bar' scaffolding plugin."
+echo
 
-valid="^[A-Z]*[a-z0-9]*( [A-Z]*[a-z0-9]*)*$"
-if [[ ! "$name" =~ $valid ]]; then
-	echo "Malformed name argument '$name'. Please use title case words separated by spaces. No hyphens."
-	exit 1
+echo -n "Enter your plugin name and press [ENTER]: "
+read name
+
+# Validate plugin name.
+if ! valid_name "$name"; then
+	echo "Malformed name '$name'. Please use title case words separated by spaces. No hyphens. For example, 'Hello World'."
+	echo
+	echo -n "Enter a valid plugin name and press [ENTER]: "
+	read name
+
+	if ! valid_name "$name"; then
+		echo
+		echo "The name you entered is invalid, rage quitting."
+		exit 1
+	fi
 fi
 
 slug="$( echo "$name" | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g' )"
 prefix="$( echo "$name" | tr '[:upper:]' '[:lower:]' | sed 's/ /_/g' )"
 namespace="$( echo "$name" | sed 's/ //g' )"
 class="$( echo "$name" | sed 's/ /_/g' )"
+repo="$slug"
+
+echo
+echo "The Organization name will be converted to lowercase for use in the repository "
+echo "path (i.e. XWP becomes xwp)."
+echo -n "Enter your GitHub organization name, and press [ENTER]: "
+read org
+
+org_lower="$( echo "$org" | tr '[:upper:]' '[:lower:]' )"
+
+echo
+echo -n "Do you want to prepend 'wp-' to your repository name? [Y/N]: "
+read prepend
+
+if [[ "$prepend" != Y ]] && [[ "$prepend" != y ]]; then
+	echo
+	echo -n "Do you want to append '-wp' to your repository name? [Y/N]: "
+    read append
+
+	if [[ "$append" == Y ]] || [[ "$append" == y ]]; then
+		repo="${slug}-wp"
+	fi
+else
+	repo="wp-${slug}"
+fi
+
+echo
+echo -n "Do you want to push the plugin to your GitHub repository? [Y/N]: "
+read push
+
+echo
 
 cwd="$(pwd)"
 cd "$(dirname "$0")"
@@ -39,58 +80,63 @@ src_repo_path="$(pwd)"
 cd "$cwd"
 
 if [[ -e $( basename "$0" ) ]]; then
-	echo "Moving up one directory outside of foo-bar"
+    echo
+	echo "Moving up one directory outside of 'wp-foo-bar'"
 	cd ..
 fi
 
-if [ -e "$slug" ]; then
+if [[ -e "$slug" ]]; then
+    echo
 	echo "The $slug directory already exists"
-	exit
+	exit 1
 fi
 
-echo "Name: $name"
-echo "Slug: $slug"
-echo "Prefix: $prefix"
-echo "NS: $namespace"
-echo "Class: $class"
+echo
 
-git clone "$src_repo_path" "$slug"
+git clone "$src_repo_path" "$repo"
 
-cd "$slug"
+cd "$repo"
 
 git mv foo-bar.php "$slug.php"
-cd tests
-git mv class-test-foo-bar.php "class-test-$slug.php"
-cd ..
+git mv tests/phpunit/class-test-foo-bar.php "tests/phpunit/class-test-$slug.php"
 
-git grep -lz "xwp/wp-foo-bar" | xargs -0 sed -i '' -e "s/xwp\/wp-foo-bar/$user\/$slug/g"
-git grep -lz "xwp" | xargs -0 sed -i '' -e "s/xwp/$user/g"
-git grep -lz "$user/wp-dev-lib" | xargs -0 sed -i '' -e "s/$user\/wp-dev-lib/xwp\/wp-dev-lib/g"
-git grep -lz "vendor/$user" | xargs -0 sed -i '' -e "s/vendor\/$user/vendor\/xwp/g"
-git grep -lz "$user/wordpress-tests-installer" | xargs -0 sed -i '' -e "s/$user\/wordpress-tests-installer/xwp\/wordpress-tests-installer/g"
-git grep -lz "$user.co" | xargs -0 sed -i '' -e "s/$user.co/xwp.co/g"
+git grep -lz "xwp" | xargs -0 sed -i '' -e "s/xwp/$org_lower/g"
+git grep -lz "wp-foo-bar" | xargs -0 sed -i '' -e "s/wp-foo-bar/$repo/g"
 git grep -lz "Foo Bar" | xargs -0 sed -i '' -e "s/Foo Bar/$name/g"
 git grep -lz "foo-bar" | xargs -0 sed -i '' -e "s/foo-bar/$slug/g"
 git grep -lz "foo_bar" | xargs -0 sed -i '' -e "s/foo_bar/$prefix/g"
 git grep -lz "FooBar" | xargs -0 sed -i '' -e "s/FooBar/$namespace/g"
 git grep -lz "Foo_Bar" | xargs -0 sed -i '' -e "s/Foo_Bar/$class/g"
 
+# Clean slate.
 rm -rf .git
+rm -rf node_modules
 rm -rf vendor
-rm -f composer.lock
 rm -f init-plugin.sh
+rm -f composer.lock
+rm -f package-lock.json
 
+# Setup Git.
 git init
-git add -A .
+git add .
 git commit -m "Initial commit"
-git remote add origin "git@github.com:$user/$slug.git"
-git push -u origin master
+git remote add origin "git@github.com:$org_lower/$repo.git"
 
-composer install
-
-if [ ! -f .git/hooks/pre-commit ]; then
-    vendor/xwp/wp-dev-lib/scripts/install-pre-commit-hook.sh;
+# Install dependencies.
+if [[ -f package.json ]]; then
+    npm install
+else
+    composer install
 fi
 
+if [[ "$push" == Y ]] || [[ "$push" == y ]]; then
+    git push -u origin master
+else
+    echo
+    echo "Push changes to GitHub with the following command:"
+    echo "cd $(pwd) && git push -u origin master"
+fi
+
+echo
 echo "Plugin is located at:"
 pwd
