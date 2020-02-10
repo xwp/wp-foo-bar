be60a928b7fc2e3ffc46fc0f57ed9428a563b8d4
Derek Herman <derek.herman@xwp.co>
2020-02-09 18:50:51 -0800

    Fix style & path

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  verbose: true,
-  collectCoverageFrom: [
-	"assets/src/**",
-	"!**/polyfills/**"
-  ],
-  testPathIgnorePatterns: [
-	"/bin/",
-	"/build/",
-    "/node_modules/",
-	"/vendor/"
-  ]
+	verbose: true,
+	collectCoverageFrom: [
+		"assets/src/**",
+		"!**/polyfills/**"
+	],
+	testPathIgnorePatterns: [
+		"/bin/",
+		"/build/",
+		"/node_modules/",
+		"/vendor/"
+	]
 };
