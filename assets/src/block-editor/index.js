86b4584b0a93791d27bc9a3b0ba337512e7cd1b9
Derek Herman <derek.herman@xwp.co>
2020-02-26 23:53:59 -0800

    Hello World block with spec

--- assets/src/block-editor/index.js
+++ assets/src/block-editor/index.js
@@ -1,10 +1,26 @@
 // Add the JS code to this file. On running npm run dev, it will compile to assets/js/.
 
 /**
+ * WordPress dependencies
+ */
+import { registerBlockType } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
 import './edit.css';
 
-export function add( to, howMuch ) {
-	return to + howMuch;
-}
+registerBlockType( 'foo-bar/hello-world', {
+	title: __( 'Hello World', 'foo-bar' ),
+	icon: 'smiley',
+	category: 'common',
+
+	edit() {
+		return <h1>{ __( 'Hello Editor', 'foo-bar' ) }</h1>;
+	},
+
+	save() {
+		return <h1>{ __( 'Hello Website', 'foo-bar' ) }</h1>;
+	},
+} );
