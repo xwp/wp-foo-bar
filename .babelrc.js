b0e37965e601a5b62007734f1a3138c1e12efd65
Ravi Chandra <ravichandrach1@gmail.com>
2020-05-05 19:42:39 +0530

    Add e2e coverage deps and scripts

--- .babelrc.js
+++ .babelrc.js
@@ -9,5 +9,11 @@ module.exports = {
 		test: {
 			plugins: [ 'transform-require-context' ],
 		},
+		teste2e: {
+			plugins: [ 'istanbul' ],
+		},
+		development: {
+			plugins: [ 'istanbul' ],
+		},
 	},
 };
